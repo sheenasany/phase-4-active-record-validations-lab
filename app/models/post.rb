5e32b68ae30627a1e1589bbbbbcef5472e5c509b
Sheena Sang <sheenasany@gmail.com>
2023-02-09 12:42:27 -0500

    commit tree 4cb8032cf4fb39553bf7c178a778c57ab1435625

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,28 @@
 class Post < ApplicationRecord
+
+validates :title, presence: true, on: :create
+validates :title, presence: true, on: :update, if: :sufficiently_clickbait_y
+validates :content, length: { minimum: 250}
+validates :summary, length: { maximum: 250}
+validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+validate :sufficiently_clickbait_y
+
+CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+]
+# /i = not case sensitive
+# \d = includes a number
+
+def sufficiently_clickbait_y
+    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+        errors.add(:title, "requires clickbait-y title")
+    end
+
+    # unless title != "Won't Believe" || "Secret" || "Top[number]" || "Guess"
+    #     errors.add(:title, "requires clickbait-y title")
+    # end
+end
 end
